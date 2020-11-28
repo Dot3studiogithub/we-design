eb47e21db443e1cb2e5b1f3fda4164ffe968ce12
Dot3studiogithub <codemonk22233@gmail.com>
2020-11-28 14:56:03 +0530

    this is my first coment

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codemountstudio</title>
+    <link rel="stylesheet" href="dist/css/bootstrap.css">
+    <link rel="stylesheet" href="dist/css/all.css">
+    <link rel="stylesheet" href="dist/css/style.css">
+</head>
+<body>
+<div class="codemount_header">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="codemount_logo">
+<img src="dist/images/logo/codemount_logo.png" alt="codemountstudio">
+</div>
+</div>
+</div>
+</div>
+<!-- end row -->
+
+
+<!--navigation bar start-->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light codemount_navbar">
+   
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#codemountnavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="codemountnavbar">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link codemount_link current" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link codemount_link" href="#">Blog</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link codemount_link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           Downloads
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">HTML AND CSS TUTORILS</a>
+            <a class="dropdown-item" href="#">BOOTSTRAP TUTORIALS</a>
+            <a class="dropdown-item" href="#">PHP MYSQL TUTORIALS</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">MORE...</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link codemount_link" href="#" tabindex="-1" aria-disabled="true">services</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link codemount_link" href="#" tabindex="-1" aria-disabled="true">About us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link codemount_link" href="#" tabindex="-1" aria-disabled="true">contact us</a>
+          </li>
+      </ul>
+     
+    </div>
+  </nav>
+
+<!--end navigation bar-->
+</div>
+<!-- end header -->
+
+
+
+
+
+
+
+ <script src="dist/js/jquery.js"></script>  
+ <script src="dist/js/bootstrap.js"></script>  
+ <script src="dist/js/all.js"></script>   
+</body>
+</html>
